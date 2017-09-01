8c0567d4db98a7a4f0a5658ed79618b2d4a2fd2e
Hilton Cabrera <hdcs05@gmail.com>
2017-09-01 03:54:15 -0400

    Missing Match

--- app/public/javascript/ffsurvey.js
+++ app/public/javascript/ffsurvey.js
@@ -60,4 +60,54 @@ $(function () {
 		}
 	});
 
+	fsendform();
+
 });
+
+function fsendform() {
+
+	$("#SubmitA").on("click", function(event) {
+		event.preventDefault();
+		// Here we grab the form elements
+		var newFriend = {
+			name: $("#NameForm").val().trim(),
+			photo: $("#URLForm").val().trim(),
+			scores: [
+				$("#color1").val(),
+				$("#color2").val(),
+				$("#color3").val(),
+				$("#color4").val(),
+				$("#color5").val(),
+				$("#color6").val(),
+				$("#color7").val(),
+				$("#color8").val(),
+				$("#color9").val(),
+				$("#color10").val()
+			]
+		};
+
+		//console.log(newFriend);
+
+
+		$.post("/api/friends", newFriend, function(data) {
+
+			if (data) {
+			// 	alert("Yay! You did it");
+			// }
+
+				$(".modal-title").text("Yay! Your Color Match is "); // + data[0] );
+				$(".modal-title").css("background-color", "orange");
+				$(".modal-body").text("Coming soon");
+				//$('#myModal img').attr('src', data[1]);
+				$("#myModal").modal("show");
+			}
+			//Clear the form when submitting
+			$("#NameForm").val("");
+			$("#URLForm").val("");
+
+		});
+
+
+	});
+
+}
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,27 +2,49 @@
 var friendData = require("../data/friend");
 var vnamematch = ""
 var vimage = ""
+var ofriends = "";
+var ocurrfriend = "";
 
 module.exports = function(app) {
  
   app.get("/api/friends", function(req, res) {
-    res.json(friendData);
+	 res.json(friendData);
   });
 
   app.post("/api/friends", function(req, res) {
-      friendData.push(req.body);
-      res.json(true);
-      //fColorMatch();
-      //res.json(     data in here     );
+		friendData.push(req.body);
+		ofriends = friendData;
+		ocurrfriend = req.body;
+		res.json(true);
+		//fColorMatch();
+		//console.log(req.body);
+		//res.json([vnamematch,vimage]);
   });
  
 };
 
-// function fColorMatch() {
-//   var vtotaldif = 0;
-//   var vnum = 0;
-//   for (var i = 0; i < friendData.length; i++) {
-//     vnum = req.body.scores[i] - friendData.scores[i];
-
-//   }
-// };
+function fColorMatch() {
+	var vtotaldif = 0;
+	var vnum = 0;
+	//var ofriends = friendData;
+	var vnumcontrol = 0;
+	console.log(ofriends);
+	console.log(ocurrfriend);
+	for (var i = 0 in ofriends) {
+		for (var j = 0; j < 10; j++ ) {
+			vnum = ocurrfriend.scores[j] - ofriends.scores[j];
+			vtotaldif += Math.abs(vnum);
+		}
+		if (i == 0) {
+			vnumcontrol = vtotaldif;
+			vnamematch = ofriends.name;
+			vimage = ofriends.photo;
+		}	else {
+				if (vtotaldif < vnumcontrol) {
+					vnumcontrol = vtotaldif;
+					vnamematch = ofriends.name;
+					vimage = ofriends.photo;	
+				}
+			}
+	}
+};
